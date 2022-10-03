38487d162a9c1d5e4b6464f40ba2a32fdc59e217
llucas <lucas.scurvello@gmail.com>
2022-10-03 18:39:19 -0300

    created server.js and config package.json

---
+++ src/server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/message/:id/:user', (request, response) => {
+  const { id, user } = request.params
+
+  response.send(`
+    Mensagem ID: ${id}. 
+    Usuário: ${user}.
+  `)
+})
+
+app.get('/users', (request, response) => {
+  const { page, limit } = request.query
+
+  response.send(`Página ${page}. Mostrar: ${limit}`)
+})
+
+const PORT = 3333
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
