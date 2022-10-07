dddd0df960566e8f597ce861aefa5f7276bb1139
lucas <lucas.scurvello@gmail.com>
2022-10-07 16:28:33 -0300

    post method + response.json

--- src/server.js
+++ src/server.js
@@ -1,21 +1,13 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
+app.use(express.json());
 
-app.get('/message/:id/:user', (request, response) => {
-  const { id, user } = request.params
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
 
-  response.send(`
-    Mensagem ID: ${id}.
-    Nome do usuário: ${user}.
-  `)
-})
+  response.json({ name, email, password });
+});
 
-app.get('/users', (request, response) => {
-  const { page, limit } = request.query
-
-  response.send(`Página ${page}. Mostrar: ${limit}`)
-})
-
-const PORT = 3333
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+const PORT = 3030;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
