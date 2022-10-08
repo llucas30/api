ff1ab098d2a7fa9ce23d4f68061689c54d3a7fcb
lucas <lucas.scurvello@gmail.com>
2022-10-07 21:15:56 -0300

    add index.js on the routes folder

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
-const { userRoutes } = require("./routes/user.routes")
+const routes = require("./routes")
 
 const app = express();
 app.use(express.json());
+
+app.use(routes);
 
 const PORT = 3030;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
