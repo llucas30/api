9d29638d1716967c9cb0ffbaa5728d3a972adba6
lucas <lucas.scurvello@gmail.com>
2022-10-07 21:07:56 -0300

    add routes folder with user.routes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 
+const { userRoutes } = require("./routes/user.routes")
+
 const app = express();
 app.use(express.json());
-
-app.post("/users", (request, response) => {
-  const { name, email, password } = request.body;
-
-  response.json({ name, email, password });
-});
 
 const PORT = 3030;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
