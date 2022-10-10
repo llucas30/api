274a8c0725c06b5f7ec98bd81d894db979df4a34
lucas <lucas.scurvello@gmail.com>
2022-10-10 14:54:59 -0300

    created new folder: controller

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,12 @@
+class UsersController {
+  /* boas práticas, um padrão para seguir:
+    -> um controller pode ter até 5 funções/métodos:
+        index - GET para listar vários registros.
+        show - GET para exibir um registro específico.
+        create - POST para criar um registro.
+        update - PUT para atualizar um registro.
+        delete - DELETE para remover um registro.
+  */
+};
+
+module.exports = UsersController;
