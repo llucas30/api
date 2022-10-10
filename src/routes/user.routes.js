bb957a032d4105d2c806fe8c7ac11636ef49a28a
lucas <lucas.scurvello@gmail.com>
2022-10-10 15:53:12 -0300

    learning middleware

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,8 +4,20 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
+
+function myMiddleware(require, response, next) {
+  console.log("Você passou pelo middleware!");
+
+  next();
+}
+
+
+
+
+
+
 const usersController = new UsersController();
 
-usersRoutes.post("/", usersController.create);
+usersRoutes.post("/", myMiddleware, usersController.create);
 
 module.exports = usersRoutes;
