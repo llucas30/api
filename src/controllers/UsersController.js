8de7d45027c07442af93fef4fc54479c3a156e67
lucas <lucas.scurvello@gmail.com>
2022-10-10 16:22:40 -0300

    express-async-errors installed

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,6 +1,12 @@
+const AppError = require("../utils/AppError");
+
 class UsersController {
   create(request, response) {
     const { name, email, password } = request.body;
+
+    if(!name) {
+      throw new AppError("O nome é obrigátorio!");
+    }
 
     response.status(201).json({ name, email, password });
   }
