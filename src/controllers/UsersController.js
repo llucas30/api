88bd34a36252bb01ed27af09b7224245a9d23690
lucas <lucas.scurvello@gmail.com>
2022-10-10 15:01:31 -0300

    refactoring: method create in UsersController

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,12 +1,9 @@
 class UsersController {
-  /* boas práticas, um padrão para seguir:
-    -> um controller pode ter até 5 funções/métodos:
-        index - GET para listar vários registros.
-        show - GET para exibir um registro específico.
-        create - POST para criar um registro.
-        update - PUT para atualizar um registro.
-        delete - DELETE para remover um registro.
-  */
+  create(request, response) {
+    const { name, email, password } = request.body;
+
+    response.json({ name, email, password });
+  }
 };
 
 module.exports = UsersController;
