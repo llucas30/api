fa48c16c39e5799f29b1d17763c5ce4cc78d8a31
lucas <lucas.scurvello@gmail.com>
2022-10-10 16:03:52 -0300

    created utils folder n AppError.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,20 +4,8 @@ const UsersController = require("../controllers/UsersController");
 
 const usersRoutes = Router();
 
-
-function myMiddleware(require, response, next) {
-  console.log("Você passou pelo middleware!");
-
-  next();
-}
-
-
-
-
-
-
 const usersController = new UsersController();
 
-usersRoutes.post("/", myMiddleware, usersController.create);
+usersRoutes.post("/", usersController.create);
 
 module.exports = usersRoutes;
