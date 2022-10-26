6b62d9de95f896f2b2ac23fb29591114293f4bd5
lucas <lucas.scurvello@gmail.com>
2022-10-26 16:07:21 -0300

    encrypt passwords with bcryptjs

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,5 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/AppError");
-
 const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
@@ -12,6 +12,10 @@ class UsersController {
     if (checkUserExists) {
       throw new AppError("Este e-mail já está cadastrado.");
     }
+
+    const hashedPassword = await hash(password, 8);
+
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
 
      return response.status(201).json();
   }
