36d31d796163a8d3f749cf82a9efe65807ab3f31
lucas <lucas.scurvello@gmail.com>
2022-11-11 16:07:45 -0300

    links n tags migrations

--- knexfile.js
+++ knexfile.js
@@ -6,6 +6,10 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, "src", "database", "database.db")
     }, 
+    pool: {
+      // habilita o funcionamento do delete em cascata das tabelas
+      afterCreate: (conn, cb) => conn.run("PRAGMA foreign_keys = ON", cb)
+    }, 
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "knex", "migrations")
     }, 
